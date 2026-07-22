1a9ebf29352ca81edc36e2b0ab9a8d55dbfd0f35
Her bunheng <hengnun68@gmail.com>
2026-07-22 09:53:02 +0700

    ..

--- src/middlewares/apiKey.js
+++ src/middlewares/apiKey.js
@@ -1,14 +1,8 @@
 const apiKey = (req, res, next) => {
   const expectedKey = process.env.API_KEY;
-
-  // If no API_KEY environment variable is configured on the server, skip authentication
-  if (!expectedKey) {
-    return next();
-  }
-
   const clientKey = req.headers['x-api-key'] || req.query.api_key;
 
-  if (!clientKey || clientKey !== expectedKey) {
+  if (!expectedKey || !clientKey || clientKey !== expectedKey) {
     return res.status(401).json({
       error: 'Unauthorized: Invalid or missing API key.'
     });
